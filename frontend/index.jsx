e492536e737658ba34d65c75a738cae9903a3845
Scott Ha <123viet123@gmail.com>
2020-03-24 17:44:45 -0700

    Added click handler for bases

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -14,6 +14,44 @@ document.addEventListener("DOMContentLoaded", () => {
   canvas.width = game.settings.width;
   canvas.height = game.settings.height;
 
+  canvas.addEventListener('click', event => {
+    const x = event.pageX - canvas.offsetLeft;
+    const y = event.pageY - canvas.offsetTop;
+
+    let clickedBase = false;
+
+    game.bases.forEach(base => {
+      if (y > base.y && y < base.y + base.width && x > base.x && x < base.x + base.width) {
+        console.log('Clicked a Base!', base);
+        base.selected = true;
+        game.prevClick = true;
+
+        clickedBase = true;
+      } else {
+        base.selected = false;
+      }
+    });
+
+    if (!clickedBase) {
+      console.log('Did not click a base!');
+      game.prevClick = false;
+    }
+  });
+
+  game.addBase({
+    unitCount: 50,
+    color: '#91EB8F',
+    position: [100,100],
+    growthRate: 0.1,
+  });
+
+  game.addBase({
+    unitCount: 50,
+    color: '#EB7261',
+    position: [400,400],
+    growthRate: 0.1,
+  });
+
   const context = canvas.getContext('2d');
   new GameView(game, context).start();
 });
